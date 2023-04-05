4ebee94346a8164a86c7ccd4da8a28255d2bb87b
miamauro <miiamauro@outlook.com>
2023-04-05 10:55:22 -0500

    added model files to begin models

---
+++ models/Thought.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+  },
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
