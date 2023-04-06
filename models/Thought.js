5e4f3579352066bdc428dc328b56c2dc4b7dda1d
miamauro <miiamauro@outlook.com>
2023-04-06 14:58:51 -0500

    began constructing models

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,27 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({});
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    // Default to current timestamp
+    // Use a getter method to format the timestamp on query
+  },
+  username: {
+    // The user that created this thought
+    // type: String,
+    // required: true
+  },
+  reactions: [reactionSchema],
+});
+
+// Create a virtual called reactionCount that retrieves the lengt of the thought's reactions array field on query.
 
 const Thought = model("thought", thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -3,7 +3,19 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    unique: true,
+    required: true,
+    trim: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+  },
+  // thoughts:
+  // friends:
+  // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 });
 
 const User = model("user", userSchema);
