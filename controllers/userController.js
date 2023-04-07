64375f3a8c9e72a6493920da3b4634329453cce9
miamauro <miiamauro@outlook.com>
2023-04-07 02:30:11 -0500

    fixed bug in deleteFriend function within userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,17 +68,17 @@ module.exports = {
       .catch((error) => res.status(500).json(error));
   },
   // Delete a friend by ID.
-  // deleteFriend() {
-  //   User.findOneAndUpdate(
-  //     { _id: req.params.userId },
-  //     { $pull: { friends: { friendId: req.params.friendId } } },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((user) =>
-  //       !user
-  //         ? res.status(404).json({ message: "No user with this ID." })
-  //         : res.json({ message: "Friend has been deleted." })
-  //     )
-  //     .catch((error) => res.status(500).json(error));
-  // },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID." })
+          : res.json({ message: "Friend has been deleted." })
+      )
+      .catch((error) => res.status(500).json(error));
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,7 +16,10 @@ router.route("/").get(getUsers).post(addUser);
 // http://localhost:3001/api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// http://localhost:3001/api/users/:userId/friends
+router.route("/:userId/friends").post(addFriend);
+
 // http://localhost:3001/api/users/:userId/friends/:friendId
-// router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
